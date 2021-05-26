213ed5ed3bfc2385a9b1abf55e617ecf0c2d47ce
Joel Auterson <joel.auterson@gmail.com>
2021-05-26 21:39:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# tmux-spotify-info
+
+This is an alternative version of [tmux-spotify-info](https://github.com/jdxcode/tmux-spotify-info)
+which uses DBus (suitable for Linux systems) rather than Applescript.
+
+It uses some stuff from [sp](https://gist.github.com/wandernauta/6800547), which
+is a more comprehensive DBus interface you should check out.
---
+++ tmux-spotify-info.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function require {
+	hash $1 2>/dev/null || {
+		echo >&2 "Error: '$1' is required, but was not found."; exit 1;
+	}
+}
+
+require dbus-send
+require grep
+
+function getMeta {
+	dbus-send --print-reply --dest="org.mpris.MediaPlayer2.spotify" \
+		"/org/mpris/MediaPlayer2" org.freedesktop.DBus.Properties.Get \
+		string:"org.mpris.MediaPlayer2.Player" string:'Metadata' \
+		| grep -Ev "^method"                           `# Ignore the first line.`   \
+		| grep -Eo '("(.*)")|(\b[0-9][a-zA-Z0-9.]*\b)' `# Filter interesting fiels.`\
+		| sed -E '2~2 a|'                              `# Mark odd fields.`         \
+		| tr -d '\n'                                   `# Remove all newlines.`     \
+		| sed -E 's/\|/\n/g'                           `# Restore newlines.`        \
+		| sed -E 's/(xesam:)|(mpris:)//'               `# Remove ns prefixes.`      \
+		| sed -E 's/^"//'                              `# Strip leading...`         \
+		| sed -E 's/"$//'                              `# ...and trailing quotes.`  \
+		| sed -E 's/"+/|/'                             `# Regard "" as seperator.`  \
+		| sed -E 's/ +/ /g'                            `# Merge consecutive spaces.`
+}
+
+META="$(getMeta)"
+TITLE="$(echo "${META}" | grep --color=never -E "(title)" | sed 's/^.*|//')"
+ARTIST="$(echo "${META}" | grep --color=never -E "(artist)" | sed 's/^.*|//')"
+
+echo "${TITLE} - ${ARTIST}"
